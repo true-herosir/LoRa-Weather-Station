72f86abf3ee5439e3adb181bf8e196b53d06b7ef
PeterSamaan <545727@student.saxion.nl>
2024-11-29 10:11:26 +0100

    Sample GUI parser

---
+++ GUI_parser/GUI_interfaces.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class bat_stat
+    {
+        public string node_id { get; set; }
+        public string location { get; set; }
+        public int? bat { get; set; }
+    }
+    internal class GUI_interfaces
+    {
+        public interface IGUI_JSON_parse
+        {
+            List<string> node_ID (string json);
+            List<DateTime> time(string json);
+            List<double?> pressure (string json);
+            List<double?> illumination (string json);
+            List<double> humidity (string json);
+            List<string> gateway_Location (string json);
+            List<double?> temperature_indor (string json);
+            List<double?> temperature_outdor (string json);
+            List<bat_stat> battery(string json);
+        }
+
+    }
+}

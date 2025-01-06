da1778d830959524bf19993f6ad5dbbc1e61e0f5
PeterSamaan <545727@student.saxion.nl>
2025-01-06 17:35:44 +0100

    backing up db

--- db backups/tables_queries.sql
+++ db backups/tables_queries.sql
@@ -3,7 +3,7 @@ USE LoRa
 
 SELECT *
   FROM [LoRa].[lr2].[Node]
-  WHERE Node_ID Like 'lht%' 
+  --WHERE Node_ID = 'mkr-saxion'
   ORDER BY Time DESC
 
 
@@ -15,19 +15,19 @@ SELECT *
 
    SELECT *
   FROM [LoRa].[lr2].[hours_avg]
-  --WHERE Node_ID = 'lht-gronau'
+  --WHERE Node_ID = 'lht-tester'
   ORDER BY the_day DESC, the_hour DESC, Location;
 
   
   SELECT *
   FROM [LoRa].[lr2].[max_min]
- --WHERE Node_ID = 'lht-wierden'
+ --WHERE Node_ID = 'lht-tester'
   ORDER BY the_day DESC, Location;
 
    SELECT *
   FROM [LoRa].[lr2].[Node_location]
 
-
+/*
 ALTER TABLE [LoRa].[lr2].[Node] DISABLE TRIGGER handle_recents
 ALTER TABLE [LoRa].[lr2].[Node] DISABLE TRIGGER trg_avg_hourly
 ALTER TABLE [LoRa].[lr2].[Node] DISABLE TRIGGER handle_outdoor_lhts
@@ -35,7 +35,7 @@ ALTER TABLE [LoRa].[lr2].[Node] DISABLE TRIGGER handle_outdoor_lhts
 ALTER TABLE [LoRa].[lr2].[Node] ENABLE TRIGGER handle_recents
 ALTER TABLE [LoRa].[lr2].[Node] ENABLE TRIGGER trg_avg_hourly
 ALTER TABLE [LoRa].[lr2].[Node] ENABLE TRIGGER handle_outdoor_lhts
-
+*/
 
   --DELETE [LoRa].[lr2].[Node]
   --WHERE Node_ID = 'lht-gronau' AND Time < '2024-12-06 10:01:33.000'
